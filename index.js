282d742946199fee052d9068c4ef11aeaf6898ee
kriti <kriti.jindal.37@gmail.com>
2023-12-14 22:41:52 +0530

    view engine ejs

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 import express from 'express'
-import ProductController from './src/controllers/product.controller.js'
+import ProductController from './src/controllers/product.controller.js';
+import ejsLayouts from 'express-ejs-layouts';
+import path from 'path';
 
 const server = express();
-const productController = new ProductController()
-server.get('/', productController.getProducts);
+
+// setup view engine settings
+server.set("view engine", "ejs");
+// path of our views
+server.set("views", path.join(path.resolve(), "src", 'views'));
+
+server.use(ejsLayouts);
+
+// create an instance of ProductController
+const productController = new ProductController();
+server.get('/', (productController.getProducts));
 
 server.use(express.static('src/views'));
 
-server.listen(3001, () => {
-    console.log("listening at port 3001");
-});
-
+server.listen(3400);
+console.log('Server is listening on port 3400');
--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,7 +1,12 @@
-import path from 'path'
+import path from 'path';
+import ProductModel from '../models/product.model.js';
 
 export default class ProductController {
+
     getProducts(req, res) {
-        return res.sendFile(path.join(path.resolve(), 'src', 'views', 'products.html'));
+        let products = ProductModel.get()
+        console.log(products);
+        res.render("products", { products: products })
+        // return res.sendFile(path.join(path.resolve(),"src",'views',"products.html" ));
     }
 }
